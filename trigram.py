93c08efc2abb2e7e3a2ed4692521071fb805f38a
Léo Dana <cargodana8@gmail.com>
2024-03-20 18:45:50 +0100

    operationnal bib for interp of the trigram

--- trigram.py
+++ trigram.py
@@ -2,18 +2,24 @@ import torch as t
 import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
+from typing import Dict
 
 n_gram = 3
 N = 5
 d = 2
-max_seq_len = n_gram*7
+max_seq_len = n_gram
 assert max_seq_len % n_gram == 0
 
 lamb = 1.5
 pi = t.softmax(lamb*t.randn(N**n_gram), dim=0)
 
 
-def best_loss(pi: t.Tensor, N: int, n_gram: int):
+def best_loss(params: Dict):
+    """Computes the best possible loss for the n_gram problem."""
+    pi = params['pi']
+    N = params['N']
+    n_gram = params['n_gram']
+
     pi_1 = t.reshape(pi, (N,)*n_gram)
 
     entropy = 0
@@ -27,12 +33,14 @@ def best_loss(pi: t.Tensor, N: int, n_gram: int):
 
 class Transformer(t.nn.Module):
     def __init__(self, d: int, N: int, nb_layer: int, max_seq_len: int, n_gram: int, pi: t.Tensor):
-        self.d = d
-        self.N = N
-        self.nb_layer = nb_layer
-        self.max_seq_len = max_seq_len
-        self.pi = pi
-        self.n_gram = n_gram
+        self.meta_params: Dict = {
+            'd': d,
+            'N': N,
+            'n_gram': n_gram,
+            'max_seq_len': max_seq_len,
+            'pi': pi,
+            'nb_layer': nb_layer,
+        }
 
         super().__init__()
 
@@ -42,16 +50,19 @@ class Transformer(t.nn.Module):
         self.unemb = t.nn.Linear(d, N, bias=False)
 
     def forward(self, x: t.Tensor):
-        assert x.shape[1] <= self.max_seq_len
+        assert x.shape[1] <= self.meta_params['max_seq_len']
+        d = self.meta_params['d']
+        N = self.meta_params['N']
+
         vect = self.word_emb.weight[x] + self.pos_emb.weight[:x.shape[1]].unsqueeze(0)
 
         for module in self.seq:
-            vect = t.concat([vect for _ in range(self.N**2)], dim=2)
+            vect = t.concat([vect for _ in range(N**2)], dim=2)
             vect = module(vect, vect, vect)[0]
             vect = t.as_strided(
                 vect,
-                (vect.shape[0], vect.shape[1], self.d, self.N**2),
-                (vect.stride()[0], vect.stride()[1], self.d, 1)
+                (vect.shape[0], vect.shape[1], d, N**2),
+                (vect.stride()[0], vect.stride()[1], d, 1),
                 ).sum(-1)
             
         logits = self.unemb(vect)
@@ -59,14 +70,18 @@ class Transformer(t.nn.Module):
     
 
 
-def generate_data(batch_size: int, num_batch: int, max_seq_len: int, N: int, pi: t.Tensor):
+def generate_data(batch_size: int, num_batch: int, params: Dict, unif=False):
+    """Generate batch_size*num_batch sequences using the n_gram distribution."""
+    max_seq_len=params['max_seq_len']
+    N=params['N']
+    n_gram=params['n_gram']
+    pi: t.Tensor =params['pi']
+    if unif:
+        pi = t.softmax(t.zeros_like(pi), dim=0)
     
     cat = t.distributions.Categorical(pi)
-    tokens = cat.sample((batch_size*num_batch, max_seq_len//3))
-    t1 = tokens%N
-    t2 = (tokens//N)%N
-    t3 = tokens//(N**2)
-    tokens = t.concat([t1.unsqueeze(1), t2.unsqueeze(1), t3.unsqueeze(1)], dim=1)
+    tokens = cat.sample((batch_size*num_batch, max_seq_len//n_gram))
+    tokens = t.concat([(tokens.unsqueeze(1)//(N**i))%N for i in range(n_gram)], dim=1)
     tokens = t.transpose(tokens, 0, 2)
     tokens = t.concat([*tokens], dim=0)
     tokens = t.transpose(tokens, 0, 1)
@@ -79,54 +94,76 @@ def generate_data(batch_size: int, num_batch: int, max_seq_len: int, N: int, pi:
     return dataloader
 
 
-def CrossEntropy(model: Transformer, input: t.Tensor, proba: t.Tensor):
-    pi = t.reshape(model.pi, (model.N,)*model.n_gram)
+def exhaustive_data(num_batch: int, params: Dict):
+    """Generates num_batch batach containing all the different sentences possible."""
+    n_gram=params['n_gram']
+    max_seq_len=params['max_seq_len']
+
+    tokens = t.arange(0, N**max_seq_len, 1)
+    tokens = t.concat([(tokens.unsqueeze(1)//(N**i))%N for i in range(n_gram)], dim=1)
+    tokens = t.concat([tokens.unsqueeze(0)]*num_batch, dim=0)
+
+    return tokens
+
+
+def CrossEntropy(params: Dict, input: t.Tensor, proba: t.Tensor): #takes longer to execute than it should!!
+    """Computes the cross-entropy in the case where we know the target distribution."""
+    max_seq_len=params['max_seq_len']
+    pi = params['pi']
+    N = params['N']
+    n_gram = params['n_gram']
+
+    pi_1 = t.reshape(pi, (N,)*n_gram)
+
+    loss = 0
+    tokens = t.as_strided(input, (n_gram, input.shape[0], max_seq_len//n_gram), (1, max_seq_len, n_gram))[:-1].flatten(1,2)
+    logits = t.log(t.as_strided(proba, (n_gram, proba.shape[0], max_seq_len//n_gram), (1, max_seq_len, n_gram))).flatten(1,2).unsqueeze(-1)
+
+    target_dist = []
+    for i in range(1, n_gram):
+        pi_2 = pi_1.squeeze() #distribution of token up to n_gram-i+1
+        pi_1 = pi_2.sum(-1, keepdim=True) #distribution of token up to n_gram-i
+        target_dist.append((pi_2/pi_1)[*tokens[:(n_gram-i)]].squeeze().unsqueeze(0)) #distribution of tokens n_gram-i+1 knowing n_gram-i
+
+    target_dist.append(pi_1.squeeze().unsqueeze(0).unsqueeze(0)[:, t.zeros(input.shape[0]*max_seq_len//n_gram, dtype=t.int), :]) #distribution of the first tokens, pi_1 is constant
+    target_proba: t.Tensor = t.cat(target_dist, dim=0)
+    loss -= (target_proba*logits).sum()
+
     nb_elements = proba.shape[0]*proba.shape[1]
-
-    pi1 = pi.sum((1,2), keepdim=True)
-    pi2 = pi.sum(2, keepdim=True)
-    pi3 = pi
-
-    t1 = input[:, t.arange(0, model.max_seq_len, 3)].flatten()
-    t2 = input[:, t.arange(1, model.max_seq_len, 3)].flatten()
-
-    prob1 = (pi2/pi1)[t1]
-    prob2 = (pi3/pi2)[t1, t2]
-    prob3 = pi1
-    
-    loss1 = t.log(proba[:, t.arange(0, model.max_seq_len, 3)].flatten(0,1))*prob1.squeeze()
-    loss2 = t.log(proba[:, t.arange(1, model.max_seq_len, 3)].flatten(0,1))*prob2.squeeze()
-    loss3 = t.log(proba[:, t.arange(2, model.max_seq_len-1, 3)].flatten(0,1))*prob3.squeeze()
-
-    return -(loss1.sum() + loss2.sum() + loss3.sum())/nb_elements
+    return loss/nb_elements
 
 
 
-def train(model: Transformer, lr=1e-4, epochs=5, batch_size=2**8, num_batch=200, train_pi=False):
+def train(model: Transformer, lr=1e-4, epochs=5, batch_size=2**8, num_batch=200, train_pi=False, opt_batch=False):
     optimizer = t.optim.Adam(model.parameters(), lr=lr)
 
-    dataloader = generate_data(batch_size, num_batch, model.max_seq_len, model.N, model.pi)
-    b_loss = best_loss(model.pi, model.N, model.n_gram)
+    if opt_batch and (model.meta_params['n_gram'] == model.meta_params['max_seq_len']):
+        dataloader = exhaustive_data(num_batch, model.meta_params)
+    else:
+        dataloader = generate_data(batch_size, num_batch, model.meta_params, unif=train_pi)
+    b_loss = best_loss(model.meta_params)
 
     Loss = []
     th_Loss = []
     for _ in tqdm(range(epochs)):
 
         for batch in dataloader:
-
             model_logits = model(batch[:, :-1])
             model_proba = t.softmax(model_logits, dim=-1)
-            next_token = batch[:, 1:]
-            next_token_proba = t.zeros(next_token.shape + (model.N,))
-            next_token_proba[:, next_token] += 1
 
             if train_pi:
-                loss = CrossEntropy(model, batch[:, :-1], model_proba)
+                loss = CrossEntropy(model.meta_params, batch[:, :-1], model_proba)
             else:
+                next_token = batch[:, 1:]
+                next_token_proba = t.zeros((next_token.shape[1], next_token.shape[0], model.meta_params['N']))
+                next_token_proba[:, next_token] += 1
+                next_token_proba = t.transpose(next_token_proba, 0, 1)
+
                 loss = -(t.log(model_proba)*next_token_proba).sum(-1).mean()
                 
-                th_loss = CrossEntropy(model, batch[:, :-1], model_proba)
+                th_loss = CrossEntropy(model.meta_params, batch[:, :-1], model_proba)
                 th_Loss.append((th_loss/b_loss-1).item())
+
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
@@ -139,11 +176,15 @@ def train(model: Transformer, lr=1e-4, epochs=5, batch_size=2**8, num_batch=200,
 model = Transformer(d, N, 1, max_seq_len, n_gram, pi)
 dict = train(model, train_pi=True)
 
-plt.plot(dict['Loss'], label='dist pred')
+plt.plot(dict['Loss'], label='train_pi')
 
 model = Transformer(d, N, 1, max_seq_len, n_gram, pi)
 dict = train(model)
 
-plt.plot(dict['Loss'], label='next token pred')
-plt.plot(dict['th_Loss'], label='next token dist pred')
+plt.plot(dict['th_Loss'], label='base')
+
+model = Transformer(d, N, 1, max_seq_len, n_gram, pi)
+dict = train(model, train_pi=True, opt_batch=True)
+
+plt.plot(dict['Loss'], label='opt_batch')
 plt.show()
